5cb9b16813c69ef5a997981baaa5587f97bb8d7c
chintan <chintansutariya322@gmail.com>
2020-07-07 19:18:32 +0530

    Natural Language Processing

--- 7 Natural-Language-Processing/Section 33 - Natural Language Processing/Python/natural_language_processing_practice.py
+++ 7 Natural-Language-Processing/Section 33 - Natural Language Processing/Python/natural_language_processing_practice.py
@@ -5,6 +5,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 #import dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
+
+#Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+    
+#Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:,1].values
+
+#spliting the data into Training set and Testing set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+#Fitting Classifier to the training dataset
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+#Predicting the Testset result
+y_pred = classifier.predict(X_test)
+
+#Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
